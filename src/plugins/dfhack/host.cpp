91fe7012159bf495356ad93f86c4cef9007a1d61
Marcus Comstedt <marcus@mc.pp.se>
2015-02-25 22:09:36 +0100

    DFHack: Change cwd to the installed directory, setup documentDirectory, and run config.lua

--- src/plugins/dfhack/host.cpp
+++ src/plugins/dfhack/host.cpp
@@ -1,6 +1,7 @@
 #include <moai_config.h>
 #include <zl-util/headers.h>
 #include <moai-core/headers.h>
+#include <moai-sim/headers.h>
 
 #include <dfhack/host.h>
 #include <dfhack/DFHack.h>
@@ -25,5 +26,28 @@ void DFHackContextInitialize () {
     sIsInitialized = true;
   }
   REGISTER_LUA_CLASS ( DFHack )
+
+  char buf[PATH_MAX+1];
+  ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf)-1);
+  if (len > 0) {
+    buf[len] = 0;
+    char *p = strrchr(buf, '/');
+    if (p && p != buf) {
+      *p = 0;
+      zl_chdir(buf);
+    }
+  }
+
+  char *docDir = getenv("XDG_DATA_HOME");
+  if (docDir == NULL) {
+    const char *home = getenv("HOME");
+    if (home == NULL)
+      home = "/tmp";
+    snprintf(buf, sizeof(buf), "%s/%s", home, ".local/share");
+    docDir = buf;
+  }
+  MOAIEnvironment::Get ().SetValue ( MOAI_ENV_documentDirectory, docDir );
+
+  AKURunScript ( "Data/Scripts/config.lua" );
 }
 
